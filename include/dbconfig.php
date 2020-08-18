50fe130a70aa309cec7dd829ea1e914e31340681
osm313 <69797758+osm313@users.noreply.github.com>
2020-08-18 15:09:21 +0500

    Update dbconfig.php

--- include/dbconfig.php
+++ include/dbconfig.php
@@ -4,7 +4,7 @@ session_start();
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
   //Username, Password and Database
-  $con = new mysqli("localhost", "abhrfrtgcqdozn", "b09b92a24144dd0eb1fc0993795745664b077a322388ce21cd98ce185718a074", "d4ao6u9s6amfge");
+  $con = new mysqli("localhost", "ldZvMuhy0z", "uUrXofIQEd", "ldZvMuhy0z");
   $con->set_charset("utf8mb4");
 } catch(Exception $e) {
   error_log($e->getMessage());
